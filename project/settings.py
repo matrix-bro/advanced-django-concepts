5b93a08811b3e41c26fe3c16d6f769747262403f
matrix-bro <razmhz11@gmail.com>
2023-12-26 11:09:30 +0545

    Setting up Celery

--- project/settings.py
+++ project/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CELERY 
+CELERY_BROKER_URL = 'pyamqp://'
+CELERY_RESULT_BACKEND = 'rpc://'
