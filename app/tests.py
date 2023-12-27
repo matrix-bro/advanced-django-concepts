71d725338ddc63f04e113919e56a0f8c791b0933
matrix-bro <razmhz11@gmail.com>
2023-12-27 12:03:40 +0545

    Created some simple test cases

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_user_creation():
+    user = User.objects.create_user('John', 'john@gmail.com', 'password')
+
+    assert User.objects.count() == 1
+    assert user.username == 'John'
+
+def test_view(client):
+    url = reverse('index-page')
+    response = client.get(url)
+
+    assert response.status_code == 200
