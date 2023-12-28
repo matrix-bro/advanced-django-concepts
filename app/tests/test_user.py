75ba125b632337733a923154d4d787072d0d9c0c
matrix-bro <razmhz11@gmail.com>
2023-12-28 10:10:21 +0545

    Tested by creating our own custom fixture

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -1,5 +1,5 @@
 import pytest
-
+from django.contrib.auth.models import User
 
 def test_create_user(django_user_model):
     django_user_model.objects.create(username='john', password='password')
@@ -8,4 +8,23 @@ def test_create_user(django_user_model):
 
     assert user != None
     assert user.username == 'john'
+
+
+# Custom Fixture
+    
+@pytest.fixture
+def create_user(db, django_user_model):
+    def make_user(**kwargs):
+        return django_user_model.objects.create_user(**kwargs)
+    
+    return make_user
+
+
+def test_user_create_with_custom_fixture(create_user):
+    create_user(username='shira', password='password')
+
+    user = User.objects.get(username='shira')
+
+    assert user != None
+    assert user.username == 'shira'
 
